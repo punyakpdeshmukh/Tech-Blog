a661b4f8233bc827b0aaebef7e993b9c07eda497
shivanishimpi <shivanishimpi9@gmail.com>
2020-10-30 04:07:06 +0530

    ✨ Sample

---
+++ js-only.js
@@ -0,0 +1,9 @@
+const button  = document.querySelector('button')
+const input1  = document.getElementById('num1')
+const input2  = document.getElementById('num2')
+function sum(num1,num2){
+    return num1+num2;
+}
+button.addEventListener('click', function(){
+    console.log(sum(input1.value,input2.value));
+});

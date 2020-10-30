8d0e0c28c5cb0c098907181c16d7be2feda80977
shivanishimpi <shivanishimpi9@gmail.com>
2020-10-30 16:17:51 +0530

    📱 app.js update

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 //console.log('Time to code!!');
-function sum(n1, n2) {
-    return n1 + n2;
+function sum(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
 }
-var number1 = 5;
+var number1 = 7;
 var number2 = 1.8;
-var result = sum(number1, number2);
+var printResult = true;
+var phrase = "Output: ";
+var result = sum(number1, number2, printResult, phrase);
 console.log(result);
--- app.ts
+++ app.ts
@@ -1,9 +1,17 @@
 //console.log('Time to code!!');
-function sum(n1:number,n2:number){
-    return n1+n2;
+function sum(n1: number, n2: number, showResult: boolean, phrase: string) {
+  const result = n1 + n2;
+
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
 }
 
-const number1 = 5;
+const number1 = 7;
 const number2 = 1.8;
-const result= sum(number1,number2)
-console.log(result)
+const printResult = true;
+const phrase = "Output: ";
+const result = sum(number1, number2, printResult, phrase);
+console.log(result);

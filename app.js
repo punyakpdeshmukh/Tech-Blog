190693b0d78966642fbfd674e2491d4cc526c5a7
shivanishimpi <shivanishimpi9@gmail.com>
2020-10-30 17:27:16 +0530

    🧩 app.ts update

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
-var person = {
-    firstname: 'Sunshine',
-    lastname: 'Cookie'
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
+/*const ADMIN = 0;
+const READ_ONLY = 1;
+const AUTHOR = 2;
+*/
+var person /*: {
+  firstname: string;
+  lastname: string;
+} */ = {
+    firstname: "Sunshine",
+    lastname: "Cookie",
+    hobbies: ['Dance', 'Code', 'Paint'],
+    role: Role.AUTHOR
 };
+person.hobbies.push('Writing');
+//person.hobbies[0]='Live';
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toLowerCase());
+}
 console.log(person);
-console.log(person.firstname);
+console.log(person.hobbies);
+if (person.role === Role.ADMIN) {
+    console.log('isAdmin');
+}
+else {
+    console.log('is not ADMIN');
+}
--- app.ts
+++ app.ts
@@ -1,6 +1,29 @@
-const person={
-    firstname:'Sunshine',
-    lastname:'Cookie'
+enum Role {ADMIN, READ_ONLY, AUTHOR};
+/*const ADMIN = 0;
+const READ_ONLY = 1;
+const AUTHOR = 2;
+*/
+const person /*: {
+  firstname: string;
+  lastname: string;
+} */ = {
+  firstname: "Sunshine",
+  lastname: "Cookie",
+  hobbies:['Dance','Code','Paint'],
+  role : Role.AUTHOR
+};
+
+person.hobbies.push('Writing');
+//person.hobbies[0]='Live';
+
+for(const hobby of person.hobbies){
+    console.log(hobby.toLowerCase());
 }
-console.log(person)
-console.log(person.firstname)
+console.log(person);
+console.log(person.hobbies);
+if (person.role===Role.ADMIN){
+    console.log('isAdmin');
+}
+else{
+    console.log('is not ADMIN')
+}
